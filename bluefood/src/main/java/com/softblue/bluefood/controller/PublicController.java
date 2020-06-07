55e2055bd2c3d67f141ecf22282415daaaee98a2
Handersonn <handmartins@gmail.com>
2020-06-07 01:46:24 -0300

    Montando a tela de cadastro de cliente

--- bluefood/src/main/java/com/softblue/bluefood/controller/PublicController.java
+++ bluefood/src/main/java/com/softblue/bluefood/controller/PublicController.java
@@ -1,7 +1,9 @@
 package com.softblue.bluefood.controller;
 
 
+import com.softblue.bluefood.domain.cliente.Cliente;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PublicController {
 
     @GetMapping("/cliente/new")
-    public String newClient(){
+    public String newClient(Model model){
 
+        Cliente cliente = new Cliente();
+        model.addAttribute("cliente", cliente);
         return "cliente-cadastro";
     }
 }
